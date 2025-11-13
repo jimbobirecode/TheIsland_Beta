aecdc5e0902236b46f89b740d1e4b8ec703aceb7
jimbobirecode <jamiekenny08@gmail.com>
2025-11-13 22:39:49 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""
+Entry point for The Island Golf Club Email Bot
+This file imports the Flask app for Gunicorn
+"""
+
+from island_email_bot import app
+
+if __name__ == '__main__':
+    import os
+    port = int(os.getenv('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
